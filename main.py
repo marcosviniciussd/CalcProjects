d694a40d22c2828bafa31a888dd8d753536b0dbc
Marcos Vinicius <eng.comp121@gmail.com>
2017-01-16 22:09:50 -0200

    Segundo Commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 valor1 = 10
-valor2 = 15;
+valor2 = 15
+media = 0;
 resultado = 0
 
 resultado = valor1 + valor2
+media = ((valor1 + valor2) / 2)
 
 print("Resultado: ", resultado)
+print("Média: ", media)
