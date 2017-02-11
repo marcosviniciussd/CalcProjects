8eef2fb50e9bc1ce2a4bc0516b90beb0e6fe6876
Marcos Vinicius <eng.comp121@gmail.com>
2017-02-11 20:16:12 -0200

    Decimo Sexto Commit

--- Exercicios05.py
+++ Exercicios05.py
@@ -4,10 +4,17 @@ def main():
     print(mensagem)
 
 def vogal(letra):
-    if(letra == 'a' or letra == "e" or letra == "i" or letra == "o" or letra == "u"):
-        return "True"
+    verdadeiro = True
+    falso = False
+
+    if(letra == 'a' or letra == "A" 
+    or letra == "e" or letra == "E" 
+    or letra == "i" or letra == "I"
+    or letra == "o" or letra == "O" 
+    or letra == "u" or letra == "U"):
+        return verdadeiro
 
     else:
-        return "False"
+        return falso
 
 main()
